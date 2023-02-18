298708096168d40fa5de8ef745f3ff271fe30c26
WinnietheDEV <WorldWhyWin@gmail.com>
2023-02-18 23:19:54 +0700

    second commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use("/api/v1/lists", ListsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
